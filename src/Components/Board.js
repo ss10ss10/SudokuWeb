3fdeb96e80c8aede5f44fed2dbb93722a4c45d40
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-18 02:01:39 +0500

    Initial Commit

---
+++ src/Components/Board.js
@@ -0,0 +1,109 @@
+import '../Styles/Board.css';
+
+const Board = () => {
+    return (
+        <div id="table">
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>  
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+            <div className='tr'>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+                <div className='td'></div>
+            </div>
+        </div>
+    );
+}
+
+export default Board;
