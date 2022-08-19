e4af5ebbf4a131411581383a42adc285719d14ef
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-20 00:53:57 +0500

    .

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,12 +1,26 @@
 import '../Styles/Board.css';
 import Row from './Row';
+import { useState } from 'react';
+import Game from './Game';
 
 const Board = () => {
-    const boardList = Array.from(Array(9), () => new Array(9));
-    boardList[0][1] = 3;
-    boardList[0][4] = 7;
-    boardList[0][5] = 9;
-    boardList[0][6] = 1;
+    
+
+
+    // const boardList = Array.from(Array(9), () => new Array(9));
+    
+    // Tried Normal component but couldn't get it to work
+    // const boardList = <Game game={1} />;
+    // console.log(boardList);
+    
+    // Tried hook but couldn't get it to work
+    // const [boardList, setBoardList] = useState(Array.from(Array(9), () => new Array(9).fill(0)));
+    // <Game game={1} />;
+
+    // boardList[0][1] = 3;
+    // boardList[0][4] = 7;
+    // boardList[0][5] = 9;
+    // boardList[0][6] = 1;
     return (
         <div id="table">
             <Row {...boardList[0]}/>
