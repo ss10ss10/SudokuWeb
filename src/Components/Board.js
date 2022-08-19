fea93855f1bacb1f7135dbfd1bb4bfcd19bf2cb8
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-20 02:23:32 +0500

    .

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -14,15 +14,18 @@ const Board = () => {
     // console.log(boardList);
     
     // Tried hook but couldn't get it to work
-    // const [boardList, setBoardList] = useState(Array.from(Array(9), () => new Array(9).fill(0)));
-    // <Game game={1} />;
-
+    const [boardList, setBoardList] = useState();
+    const updateGame = b => {
+        setBoardList(b);
+    }
+    console.log(boardList);
     // boardList[0][1] = 3;
     // boardList[0][4] = 7;
     // boardList[0][5] = 9;
     // boardList[0][6] = 1;
     return (
         <div id="table">
+            <Game updateGame={updateGame} game />;
             <Row {...boardList[0]}/>
             <Row {...boardList[1]}/>
             <Row {...boardList[2]}/> 
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,6 +1,4 @@
-import setBoardList from './Board';
-
-const Game = (props) => {
+const Game = ({props}) => {
     const game1 = [[4, 3, 5, 2, 6, 9, 7, 3, 1], [6, 8, 2, 5, 7, 1, 4, 9, 3], [1, 9, 7, 8, 3, 4, 5, 6, 2],
                   [8, 2, 6, 1, 9, 5, 3, 4, 7], [3, 7, 4, 6, 8, 2, 9, 1, 5], [9, 5, 1, 7, 4, 3, 6, 2, 8],
                   [5, 1, 9, 3, 2, 6, 8, 7, 4], [2, 4, 8, 9, 5, 7, 1, 3, 6], [7, 6, 3, 4, 1, 8, 2, 5, 9]];
@@ -21,24 +19,23 @@ const Game = (props) => {
                   [4, 6, 8, 1, 2, 7, 3, 9, 5], [5, 9, 7, 4, 3, 8, 6, 2, 1], [1, 3, 2, 5, 9, 6, 4, 8, 7],
                   [3, 2, 5, 7, 8, 9, 1, 4, 6], [6, 4, 1, 2, 5, 3, 8, 7, 9], [7, 8, 9, 6, 4, 1, 5, 3, 2]];
 
-    let final;
-    // if (props.game === 1) {
-    //     // final = game1;
-    // } else if (props.game === 2) {
-    //     final = game2;
-    // } else if (props.game === 3) {
-    //     final = game3;
-    // } else if (props.game === 4) {
-    //     final = game4;
-    // } else if (props.game === 5) {
-    //     final = game5;
-    // }
-    // else {
-    //     final = Array.from(Array(9), () => new Array(9));
-    // }
-    setBoardList(game1);
+    
+    if (props.game === 1) {
+        props.updateGame(game1);
+    } else if (props.game === 2) {
+        props.updateGame(game2);
+    } else if (props.game === 3) {
+        props.updateGame(game3);
+    } else if (props.game === 4) {
+        props.updateGame(game4);
+    } else if (props.game === 5) {
+        props.updateGame(game5);
+    }
+    else {
+        props.updateGame(Array.from(Array(9), () => new Array(9).fill(0)));
+    }
     return (
-        <div></div>
+        <></>
     );
 }
 
