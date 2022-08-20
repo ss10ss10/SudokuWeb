e0cac883d1dc3549de7f035865498dc3501d058c
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-21 02:26:22 +0500

    .

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,9 +4,10 @@ import Game from "./Game";
 
 const Board = () => {
     const boardList = Game(3);
+    let gameBoard = Game(3);
         return (
             <div id="table">
-                <Row array={boardList[0]} cls="" />
+                {/* <Row array={boardList[0]} cls="" />
                 <Row array={boardList[1]} cls="" />
                 <Row array={boardList[2]} cls="trr" />
                 <Row array={boardList[3]} cls="" />
@@ -14,7 +15,16 @@ const Board = () => {
                 <Row array={boardList[5]} cls="trr" />
                 <Row array={boardList[6]} cls="" />
                 <Row array={boardList[7]} cls="" />
-                <Row array={boardList[8]} cls="" />
+                <Row array={boardList[8]} cls="" /> */}
+                <Row array={gameBoard[0]} cls="td" />
+                <Row array={gameBoard[1]} cls="td" />
+                <Row array={gameBoard[2]} cls="td trr" />
+                <Row array={gameBoard[3]} cls="td" />
+                <Row array={gameBoard[4]} cls="td" />
+                <Row array={gameBoard[5]} cls="td trr" />
+                <Row array={gameBoard[6]} cls="td" />
+                <Row array={gameBoard[7]} cls="td" />
+                <Row array={gameBoard[8]} cls="td" />
             </div>
         );
 };
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,9 +1,12 @@
 import "../Styles/Board.css";
 
 const Row = ({ array, cls }) => {
+    const handleClick = (e) => {
+        
+    }
         return (
             <div className="tr">
-                <div className={"td " + cls}>{array && array[0]}</div>
+                {/* <div className={"td " + cls}>{array && array[0]}</div>
                 <div className={"td " + cls}>{array && array[1]}</div>
                 <div className={"tdd " + cls}>{array && array[2]}</div>
                 <div className={"td " + cls}>{array && array[3]}</div>
@@ -11,9 +14,14 @@ const Row = ({ array, cls }) => {
                 <div className={"tdd " + cls}>{array && array[5]}</div>
                 <div className={"td " + cls}>{array && array[6]}</div>
                 <div className={"td " + cls}>{array && array[7]}</div>
-                <div className={"td " + cls}>{array && array[8]}</div>
+                <div className={"td " + cls}>{array && array[8]}</div> */}
+                {array?.map((item, i) => (
+                <div    key={i} 
+                        className={cls} 
+                        onClick={() => handleClick(i)}>
+                            {item}
+                </div>))}
             </div>
         );
 };
-
 export default Row;
