7e16a191107f264379ef1c1212194d028374c607
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2022-08-21 04:00:21 +0500

    Update Row.js

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -5,6 +5,9 @@ const Row = ({ updateGameBoard, array, cls, rowNo }) => {
     const [clss,setClss] = useState(["td ", "td ", "tdd ", "td ", "td ", "tdd ", "td ", "td ", "td "]);
     const val = useRef(0)
     const handleChange = (e, i) => {
+        if ( e < 0 || e > 9 || isNaN(e) === true) {
+            return 
+        }
         updateGameBoard(rowNo, i, Number(e));
     }
         return (
@@ -15,7 +18,7 @@ const Row = ({ updateGameBoard, array, cls, rowNo }) => {
                         className = { clss[i] + cls } 
                         onChange={(e) => handleChange(e.target.value, i)}
                     >
-                            <input type="number" className="inp" value={item}></input>
+                            <input className="inp" value={item}></input>
                     </div>))}
             </div>
         );
