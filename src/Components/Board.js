7b731cd7aa5dd5d95f49d84e947c54cb97e1bdc4
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2022-08-21 19:07:52 +0500

    update favicon

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -11,17 +11,17 @@ const Board = () => {
         newBoard[row][col] = value;
         setGameBoard(newBoard);
     }
-        return (
-            <div id="table">
-                {gameBoard?.map((item, i) => (
-                    i === 2 || i === 5 ? <Row key={i} updateGameBoard={updateGameBoard} array={gameBoard[i]} cls="trr" rowNo = {i}>
-                            {item}
-                    </Row> : <Row key={i} updateGameBoard={updateGameBoard} array={gameBoard[i]} cls="" rowNo = {i}>
-                            {item}
-                    </Row>
-                ))}
-                {console.log(gameBoard)}
-            </div>
-        );
+    return (
+        <div id="table">
+            {gameBoard?.map((item, i) => (
+                i === 2 || i === 5 ? <Row key={i} updateGameBoard={updateGameBoard} array={gameBoard[i]} cls="trr" rowNo = {i}>
+                        {item}
+                </Row> : <Row key={i} updateGameBoard={updateGameBoard} array={gameBoard[i]} cls="" rowNo = {i}>
+                        {item}
+                </Row>
+            ))}
+            {console.log(gameBoard)}
+        </div>
+    );
 };
 export default Board;
