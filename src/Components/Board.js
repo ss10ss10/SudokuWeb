8a37b1fbc934f942e6299437b741e19da9fc61ae
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2022-08-21 19:13:35 +0500

    fixes

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,17 +2,21 @@ import "../Styles/Board.css";
 import Row from "./Row";
 import Game from "./Game";
 import { useState } from "react";
+import { useLocation } from "react-router";
 
 const Board = () => {
-    const boardList = Game(3);
-    let [gameBoard, setGameBoard] = useState(Game(3));
+    const location = useLocation()
+    const boardList = Game( location.state?.difficulty  === undefined ? 1 : location.state.difficulty );
+
+    let [gameBoard, setGameBoard] = useState(boardList);
     const updateGameBoard = (row, col, value) => {
         let newBoard = [...gameBoard];
         newBoard[row][col] = value;
         setGameBoard(newBoard);
     }
     return (
-        <div id="table">
+        <div className="box">
+            <div id="table">
             {gameBoard?.map((item, i) => (
                 i === 2 || i === 5 ? <Row key={i} updateGameBoard={updateGameBoard} array={gameBoard[i]} cls="trr" rowNo = {i}>
                         {item}
@@ -21,6 +25,7 @@ const Board = () => {
                 </Row>
             ))}
             {console.log(gameBoard)}
+        </div>
         </div>
     );
 };
