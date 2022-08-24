8d1137417b1674227b53c3e087e8519fea192efc
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-24 22:13:01 +0500

    .

--- src/Components/displayBoard.js
+++ src/Components/displayBoard.js
@@ -14,20 +14,7 @@ const displayBoard = (dispBoard, difficulty) => {
         }
         return arr;
     }
-    
-    switch (difficulty) {
-        case 1:
-            final = removeRandom(dispBoard, difficulty * 10);
-            break;
-        case 2:
-            final = removeRandom(dispBoard, difficulty * 10);
-            break;
-        case 3:
-            final = removeRandom(dispBoard, difficulty * 10);
-            break;
-        default:
-            final = removeRandom(dispBoard, difficulty * 10);
-    }
+    final = removeRandom(dispBoard, difficulty * 10);
     return [final, emptyList];
 }
 
