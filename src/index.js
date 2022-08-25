afc18a8225b366b96bca1cb3707bccdaa49f13ab
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-25 22:22:36 +0500

    .

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App className="app-size"/>
 );
 
 // If you want to start measuring performance in your app, pass a function
